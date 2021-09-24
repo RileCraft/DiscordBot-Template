de9ae39263107d83d4c7a8b8436009d035176f0e
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-09-24 15:51:08 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const { Collection, Client, Discord, MessageEmbed } = require('discord.js')
-let prefix = process.env.prefix
-let token = process.env.token
 const { Handler } = require(`${__dirname}/Access Files/Classes/Handler`)
 const client = new Client({
     disableEveryone: true
 })
-client.login(token)
-exports.client = client
 global.rootPATH = __dirname
+client.config = require(`${rootPATH}/config.js`)[0]
+exports.client = client
 client.commands = new Collection()
 client.aliases = new Collection()
 
 Handler.loadCommands()     // LOAD COMMAND HANDLER
 Handler.loadEvents()     // LOAD EVENT HANDLER
+
+client.login(client.config.token)
