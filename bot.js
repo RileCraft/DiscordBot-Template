5bcbefdb8c278a0d2052034a4fb8f617aaf4d84d
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-09-24 15:39:19 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,14 @@ const { Collection, Client, Discord, MessageEmbed } = require('discord.js')
 let prefix = process.env.prefix
 let token = process.env.token
 const { Handler } = require(`${__dirname}/Access Files/Classes/Handler`)
-require('@weky/inlinereply')
-require('discord-reply')
 const client = new Client({
     disableEveryone: true
 })
 client.login(token)
 exports.client = client
 global.rootPATH = __dirname
-require('discord-buttons')(client)
 client.commands = new Collection()
 client.aliases = new Collection()
 
-global.db = require("quick.db")
-const env = require("dotenv").config()
-const DisTube = require("distube")
-client.distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true, leaveOnFinish: true })
-
-Handler.loadCommands()     // COMMAND HANDLER
-Handler.loadEvents()     // EVENT HANDLER
+Handler.loadCommands()     // LOAD COMMAND HANDLER
+Handler.loadEvents()     // LOAD EVENT HANDLER
