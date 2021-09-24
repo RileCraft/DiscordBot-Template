e3003807ef6a6f091b5e104fc69615f70b70ebd6
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-09-24 15:59:31 +0530

    update ready.js

--- Access Files/Events/ready.js
+++ Access Files/Events/ready.js
@@ -3,11 +3,13 @@ module.exports = {
 	once: true,
 	execute(client) {
 		const chalk = require('chalk')
-		client.user.setActivity('Cookies get baked.', {
+		client.user.setActivity('Discord Servers.', {
                 type: `WATCHING`,
             });
+            const info = require("../Classes/Handler")
     console.log(chalk.bold.yellowBright("[Bot] ") + chalk.bold.blueBright(`Connected to ${client.user.tag}`))
-    console.log(chalk.bold.blueBright("[Handler]") + chalk.bold.greenBright(` Loaded ${client.commands.size} commands.`))
-    console.log(chalk.bold.cyanBright("[Handler]") + chalk.bold.magentaBright(` Loaded ${client.aliases.size} aliases.`))
+    console.log(chalk.bold.blueBright("[Handler]") + chalk.bold.greenBright(` Loaded ${info.loadedCmds} commands.`))
+    console.log(chalk.bold.cyanBright("[Handler]") + chalk.bold.magentaBright(` Loaded ${info.loadedAliases} aliases.`))
+    console.log(chalk.bold.greenBright("[Handler]") + chalk.bold.greyBright(` Loaded ${info.loadedEvents} events.`))
 	},
 };
