ebff06e5a0a8287d2fd60815c151043fc1e65b5a
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-09 15:26:18 +0530

    update menu.js

--- Home/CMDFiles/SelectMenus/menu.js
+++ Home/CMDFiles/SelectMenus/menu.js
@@ -1,7 +1,7 @@
 module.exports = {
     name : 'fun', // Should be same as Menu Custom Id or The menu options set values.
-    run : async(client, menu) => {
-    	menu.reply("E")
+    run : async(client, interaction, Discord) => {
+    	interaction.reply("E")
     // Refer to selectMenu.js file in Commands/Examples/Menu
     }
 }     
