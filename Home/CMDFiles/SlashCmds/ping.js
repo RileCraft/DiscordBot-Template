68b9c9080acda6560065953021a9f6ef781522f2
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-09 14:40:36 +0530

    update ping.js

--- Home/CMDFiles/SlashCmds/ping.js
+++ Home/CMDFiles/SlashCmds/ping.js
@@ -6,7 +6,7 @@ module.exports = {
 	.setColor('RANDOM')
 	.setTimestamp()
         .setTitle('🏓╎Pong!')
-	.setDescription(`🏠╎Websocket Latency: ${client.ws.ping}ms\n🤖╎Bot Latency: ${Date.now() - slash.createdTimestamp}ms`);
+	.setDescription(`🏠╎Websocket Latency: ${client.ws.ping}ms\n🤖╎Bot Latency: ${Date.now() - interaction.createdTimestamp}ms`);
 	interaction.reply({ embeds: [ping] })
 		}
 	}
