f2f5204a4bd661f5637dc115448d7f2257ceeaae
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-09 14:00:24 +0530

    update ready.js

--- Home/Events/ready.js
+++ Home/Events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
                 type: `WATCHING`,
             });
             const info = require(HOME + "/Home/Classes/Handler")
-            info.Handler.loadSlashCommands(client) // SLASHCMDS HANDLER, MUST BE IN READY EVENT.
+info.Handler.loadSlashCommands(client) // SLASHCMDS HANDLER, MUST BE IN READY EVENT.
     console.log(chalk.bold.yellowBright("[Bot] ") + chalk.bold.blueBright(`Connected to ${client.user.tag}`))
     if (info.commandFiles.length > 0) console.log(chalk.bold.redBright("[Handler]") + chalk.bold.greenBright(` Loaded ${info.commandFiles.length} commands.`))
     if (info.aliasesCount.length > 0) console.log(chalk.bold.whiteBright("[Handler]") + chalk.bold.magentaBright(` Loaded ${info.aliasesCount.length} aliases.`))
