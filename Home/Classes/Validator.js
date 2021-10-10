ce7be0044a14430746973fc5d2b469eaaa6b2b54
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-10 22:11:56 +0530

    update Validator.js

--- Home/Classes/Validator.js
+++ Home/Classes/Validator.js
@@ -32,7 +32,6 @@ class validator {
                 let id = message.user.id
                 let date = Date.now()
                 let data = cooldb.get(`${id}.${command.name}.${whInt}.cooldown`)
-                if (!data) return true;
                 coolTime.push(Math.floor(Math.floor(data + time) / 1000))
                 if (isNaN(time)) throw new Error("Invalid number in cooldown provided at " + command.name)
                 if (Math.floor(date - data) >= time || !data) {
@@ -44,7 +43,6 @@ class validator {
                 let id = message.author.id
                 let date = Date.now()
                 let data = cooldb.get(`${id}.${command.name}.cooldown`)
-                if (!data) return true;
                 coolTime.push(Math.floor(Math.floor(data + time) / 1000))
                 if (isNaN(time)) throw new Error("Invalid number in cooldown provided at " + command.name)
                 if (Math.floor(date - data) >= time || !data) {
