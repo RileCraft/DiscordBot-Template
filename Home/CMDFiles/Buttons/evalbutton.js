927c83164226ac2741654146e84000b6da89107f
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-21 23:51:31 +0530

    update evalbutton.js

--- Home/CMDFiles/Buttons/evalbutton.js
+++ Home/CMDFiles/Buttons/evalbutton.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'evalbtn', // Must be same as button's Custom Id
     authorOnly: true,
+    allowBots: true,
     run: async (client, interaction, Discord) => {
         interaction.message.delete()
     }
