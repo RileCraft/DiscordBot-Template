962dd8326453c090d5bb10eeb078ddece26ef6fd
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-21 23:47:08 +0530

    update select.js

--- Home/CMDFiles/SelectMenus/select.js
+++ Home/CMDFiles/SelectMenus/select.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: "select" // Must be same as menu's customId
+	name: "select", // Must be same as menu's customId
 	expireAfter: 10000, //MS Time
 	run: async(client, interaction, Discord) => {
 		}
