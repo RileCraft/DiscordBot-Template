01cb08375168a6edf58a8c6fff4d04da9c482ba6
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-27 10:03:40 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,7 @@
 const { Collection, Client, Discord, MessageEmbed, Intents } = require('discord.js')
 const chalk = require('chalk')
 require("dotenv").config()
-const client = new Client({
-    intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_WEBHOOKS,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_INVITES,
-    Intents.FLAGS.GUILD_BANS,
-],
-partials: ["CHANNEL"]
-})
+const client = new Client()
 global.HOME = __dirname
 client.config = require(`${HOME}/config.json`)
 require('figlet')("DJS", (err, data) => console.log(data))
