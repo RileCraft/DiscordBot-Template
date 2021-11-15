5fcbfc9357fb3a8abb121a46377685e056941a83
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-11-15 18:03:04 +0530

    update Handler.js

--- Home/Classes/Handler.js
+++ Home/Classes/Handler.js
@@ -116,7 +116,7 @@ static slashCount() {
         
         } else {
         	let creator = ""
-        if (cmd?.guild) creator = client.guilds.cache.get(cmd.guild)
+        if (cmd?.guild) creator = client.guilds.cache.get(cmd.guild) ?? throw new Error("Invalid Guild ID was provided.")
         else creator = client.application
         if (creator.commands.cache.find(x => x.name == cmd.name)) creator.commands.edit(creator.commands.cache.find(x => x.name == cmd.name).id, {
                     name: cmd.name,
