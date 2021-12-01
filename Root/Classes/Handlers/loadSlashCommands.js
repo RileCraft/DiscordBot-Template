dca8cb174aecb8fb15b93e3fba8ae387cafea9c0
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-12-01 09:54:59 +0530

    update loadSlashCommands.js

--- Root/Classes/Handlers/loadSlashCommands.js
+++ Root/Classes/Handlers/loadSlashCommands.js
@@ -4,14 +4,18 @@ const fs = require("fs")
 const { FileArray } = require(`${ROOT.path}/Root/Functions/FileArray`)
 
 FileArray(`${ROOT.path}/Root/Commands/Slash`, async function(err, res) {
+	res.forEach(file => {
+		if (fs.statSync(file).isDirectory()) return;
+            let cmd = require(file)
+            if (cmd.ignoreFile) return;
+            client.commands.slash.set(cmd.name, cmd)
+            })
     let promise = Promise.resolve()
     res.forEach(function(file) {
         promise = promise.then(function() {
             const interval = 5000
             if (fs.statSync(file).isDirectory()) return;
             let cmd = require(file)
-            if (cmd.ignoreFile) return;
-            client.commands.slash.set(cmd.name, cmd)
             cmd = client.commands.slash.get(cmd.name)
 
             if (cmd.guilds && Array.isArray(cmd.guilds)) cmd.guilds.forEach(i => {
