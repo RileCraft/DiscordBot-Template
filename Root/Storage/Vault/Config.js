25fb744f0e77a2600b72b30be05b6d20034dd675
RileCraft <importfromstudio@gmail.com>
2021-12-01 08:20:50 +0530

    V4.0.1 Bug Fixes

--- Root/Storage/Vault/Config.js
+++ Root/Storage/Vault/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    prefix: ["!"],
-    developers: ["427109850368049162"],
-    token: "ODU2NDE0NzIxNzAyODg3NDI0.YNAsXQ.1yxKSjNUnLT86Ctnf05afBbZD44"
+    prefix: ["PREFIX"],
+    developers: ["OWNER USER ID"],
+    token: "TOKEN"
 }
