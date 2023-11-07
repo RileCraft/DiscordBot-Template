601a65857276fd3b0e82c220d7f9e6b332b10a7e
RileCraft <48475792+RileCraft@users.noreply.github.com>
2023-11-07 21:54:09 +0530

    Fix(Config)

--- Src/Config.mjs
+++ Src/Config.mjs
@@ -1,5 +1,5 @@
 export default  {
-    prefix: ["."],
-    botToken: "ODA3NTk0MzI4NDAyNDI3OTM1.G0fLfh.Bl3FiyAXMhHAbXyVC-VsBdPCxwa-Z4V8No4yOs",
-    ownerIds: ["427109850368049162"]
+    prefix: ["Bot Prefix"],
+    botToken: "Bot Token",
+    ownerIds: ["Bot Owner Id"]
 };;
