4a96fcb6abee296df1fa048817db7528f6ce1245
RileCraft <48475792+RileCraft@users.noreply.github.com>
2023-11-11 13:03:52 +0530

    Update SelectMenus.mjs

--- Src/Structures/Managers/SelectMenus.mjs
+++ Src/Structures/Managers/SelectMenus.mjs
@@ -1,13 +1,11 @@
 import { statSync } from "fs";
 import { glob } from "glob";
-import { join } from "path";
-import { pathToFileURL } from "url";
 
 export default async(client, rootPath) => {
     const selectMenuFiles = await glob(`${rootPath}/Src/Interactions/SelectMenus/**/*`);
     selectMenuFiles.forEach(async(selectMenuFile) => {
         if (statSync(selectMenuFile).isDirectory()) return; 
-        let selectMenuCommand = await import(pathToFileURL(join(rootPath, selectMenuFile)));
+        let selectMenuCommand = await import(selectMenuFile);
         if (selectMenuCommand.default) selectMenuCommand = selectMenuCommand.default; // Support for CommonJS
         if (selectMenuCommand.ignore || !selectMenuCommand.name || !selectMenuCommand.run) return;
 
