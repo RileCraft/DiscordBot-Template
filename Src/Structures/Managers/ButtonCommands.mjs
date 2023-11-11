1b440285fa19d5e89f0f09acbbfea25b89a3027b
RileCraft <48475792+RileCraft@users.noreply.github.com>
2023-11-11 12:59:27 +0530

    Update ButtonCommands.mjs

--- Src/Structures/Managers/ButtonCommands.mjs
+++ Src/Structures/Managers/ButtonCommands.mjs
@@ -1,13 +1,11 @@
 import { statSync } from "fs";
 import { glob } from "glob";
-import { join } from "path";
-import { pathToFileURL } from "url";
 
 export default async(client, rootPath) => {
     const buttonCommandFiles = await glob(`${rootPath}/Src/Interactions/Buttons/**/*`);
     buttonCommandFiles.forEach(async(buttonCommandFile) => {
         if (statSync(buttonCommandFile).isDirectory()) return;
-        let buttonCommand = await import(pathToFileURL(join(rootPath, buttonCommandFile)));
+        let buttonCommand = await import(buttonCommandFile);
         if (buttonCommand.default) buttonCommand = buttonCommand.default; // Support for CommonJS
         if (buttonCommand.ignore || !buttonCommand.name || !buttonCommand.run) return;
 
