a10677bd1fb5a5ffa872c6212ab1ff19d1d22aad
RileCraft <48475792+RileCraft@users.noreply.github.com>
2023-11-11 13:03:31 +0530

    Update ModalForms.mjs

--- Src/Structures/Managers/ModalForms.mjs
+++ Src/Structures/Managers/ModalForms.mjs
@@ -1,13 +1,11 @@
 import { statSync } from "fs";
 import { glob } from "glob";
-import { join } from "path";
-import { pathToFileURL } from "url";
 
 export default async(client, rootPath) => {
     const modalFormsFiles = await glob(`${rootPath}/Src/Interactions/ModalForms/**/*`);
     modalFormsFiles.forEach(async(modalFormFile) => {
         if (statSync(modalFormFile).isDirectory()) return;
-        let modalForm = await import(pathToFileURL(join(rootPath, modalFormFile)));
+        let modalForm = await import(modalFormFile);
         if (modalForm.default) modalForm = modalForm.default; // Support for CommonJS
         if (modalForm.ignore || !modalForm.name || !modalForm.run) return;
 
